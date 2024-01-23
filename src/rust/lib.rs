172754529818c288113ed1546505d229ebf39473
pillowtrucker <githubino@janitor.chat>
2024-01-23 17:34:23 +0100

    native build

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -1,11 +1,11 @@
 // license:BSD-3-Clause
 // copyright-holders:Hiromasa Tanaka
-#![no_main]
+//#![no_main]
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
-pub mod sound;
 pub mod driver;
+pub mod sound;
 pub mod wasm;
