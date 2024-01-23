39998455618b572e16572eee34fa39fcfa27ab36
pillowtrucker <githubino@janitor.chat>
2024-01-23 21:37:25 +0100

    ci

--- build.rs
+++ build.rs
@@ -4,4 +4,5 @@ fn main() {
     // export LD_LIBRARY_PATH=$(pwd)/dist
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     println!("cargo:rustc-link-search=native={}/dist", dir);
+    println!("cargo:rustc-link-search=native={}/dist/Debug", dir); // windows..
 }
