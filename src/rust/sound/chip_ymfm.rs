40787bc6eeb2f9698d444716d62ad423cac1a5de
pillowtrucker <githubino@janitor.chat>
2024-01-26 11:32:11 +0100

    dunno

--- src/rust/sound/chip_ymfm.rs
+++ src/rust/sound/chip_ymfm.rs
@@ -8,6 +8,10 @@ use super::{
 };
 use std::collections::HashMap;
 
+#[cfg_attr(
+    target_env = "msvc",
+    link(name = "msvcrt", cfg(target_feature = "crt-static"))
+)]
 #[cfg_attr(windows, link(name = "ymfm", kind = "static"))]
 #[cfg_attr(unix, link(name = "ymfm", kind = "static"))]
 //#[link(name = "ymfm")]
